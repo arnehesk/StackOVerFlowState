089dc7c4dc9ee11c6b67ef0c218d5e7c517fefdf
AndrewPierce <andrew.craig.pierce@gmail.com>
2019-10-23 09:56:58 -0700

    Added a record to static data, edited a sproc, and added a column

---
+++ Data/dbo.PostTypes_Data.sql
@@ -0,0 +1,1 @@
+INSERT INTO [dbo].[PostTypes] ([Id], [Type]) VALUES (1, N'Comment ')
--- Stored Procedures/dbo.GetPosts.sql
+++ Stored Procedures/dbo.GetPosts.sql
@@ -2,18 +2,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
---SET QUOTED_IDENTIFIER ON|OFF
---SET ANSI_NULLS ON|OFF
---RED-GATE\arneh.eskandari
---GO
-CREATE PROCEDURE [dbo].[GetPosts]
-  
--- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
-AS
-    SELECT  AnswerCount ,
-            Body ,
-            ClosedDate ,
-            CreationDate ,
-            CommentCount
-    FROM    dbo.Posts;
+CREATE   PROCEDURE [dbo].[GetPosts]
+AS BEGIN
+    SELECT * FROM Posts;
+END;
 GO
